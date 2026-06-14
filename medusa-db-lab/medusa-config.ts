2159962c9fd90020c3224bd2da7f6898e9601b23
huynp <huy.nguyen.it.work@gmail.com>
2026-06-14 23:06:07 +0700

    feat: muốn chạy được api admin #1

--- medusa-db-lab/medusa-config.ts
+++ medusa-db-lab/medusa-config.ts
@@ -14,4 +14,7 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true"
+  },
 })
